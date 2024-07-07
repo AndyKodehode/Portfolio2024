54d432cdac72c99052e499b1e14842fe5f0d3ac4
AndyKodehode <andykodehode@gmail.com>
2024-07-07 21:19:17 +0200

    bilder og tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,48 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        "2xs": "375px",
+        xs: "425px",
+        "3xl": "2600px",
+      },
+      colors: {
+        kv: {
+          black: "#0B1215",
+          white: "#FFFAFA",
+        },
+        /* Farger fra Jobloop | Kodehode Figma Design Fil */
+        portfolio: {
+          /* Hovedfarge fra Figma */
+          primary: {
+            blue: "#9ABDF1",
+            yellow: "#FFF397",
+            green: "#8EE8B7",
+          },
+          /* Nedtonet fra Figma */
+          secondary: {
+            darkBlue: "#3D5876",
+            
+          }
+         
+        },
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        //fadeIn: {
+        //  "0%": { opacity: "0" },
+        //  "100%": { opacity: "100%" },
+        //},
+      },
+      animation: {
+        //fadeIn: "fadeIn 1s forwards",
+        scroll: "scroll 35s linear infinite",
+      },
+      boxShadow: {
+        cards: "0 -5px 10px 2px rgb(0 0 0 / 0.1), 0 5px 10px 2px rgb(0 0 0 / 0.1)",
       },
     },
   },
