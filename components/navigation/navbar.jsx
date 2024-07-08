278cd803f2d29c335bc0056d2db4b61461bb5a1d
AndyKodehode <andykodehode@gmail.com>
2024-07-08 16:59:00 +0200

    tailwind fix

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -4,9 +4,25 @@
 export function NavBar(){
 
     return(
-        <div className="w-full h-40 bg-black">
-          <h1>hello</h1>
+        <div className="w-screen h-16 bg-slate-600 flex flex-row items-center justify-end md:pr-7 lg:pr-10 pr-4" >
+          <div className="md:flex flex-row gap-10 hidden" >
+              <div className="w-20 h-5 bg-white ">
 
+              </div>
+
+              <div className="w-20 h-5 bg-white ">
+
+              </div>
+
+              <div className="w-20 h-5 bg-white ">
+
+              </div>
+          </div>
+
+          <div className="block md:hidden lg:hidden w-10 h-10 bg-white ">
+
+          </div>
+          
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center min-h-screen p-0" >
+    <main className="flex flex-col items-left min-h-screen p-0 w-screen" >
       <NavBar/>
     
     </main>
