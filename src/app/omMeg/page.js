b0ecc00a6913da6eb07d616c173eb331da59dd35
AndyKodehode <andykodehode@gmail.com>
2024-08-27 13:22:15 +0200

    mobil start

--- src/app/omMeg/page.js
+++ src/app/omMeg/page.js
@@ -8,18 +8,18 @@ import Books from "../../../public/Books.svg"
 
 export default function OmMeg(){
     return(
-        <div className="w-full flex flex-row gap-20">
-            <div className="w-2/6 h-full z-10">
+        <div className="w-full flex flex-col md:flex-row gap-20 items-center">
+            <div className="md:w-2/6 w-5/6 md:h-full z-10 pt-12">
                <Image src={MegSelv}/>
             </div>
             
-            <div className="w-4/6 flex flex-col ">
+            <div className="md:w-4/6 w-full flex flex-col ">
                     <div className="w-full flex flex-col p-12 gap-7">
                         <div className="flex flex-col gap-3">
                             <h1>Hvem er jeg?</h1>
-                            <p className="lg:text-[20px] lg:pb-12 md:pb-7 p-5 md:p-0 text-center md:text-left">En rolig mann fra Bergen som brenner for design, kreativitet og utvikling</p>
+                            <p className="lg:text-[20px] lg:pb-12 md:pb-7 p-5 md:p-0 text-left">En rolig mann fra Bergen som brenner for design, kreativitet og utvikling</p>
                         </div>
-                        <div className="flex flex-row gap-4 w-full">
+                        <div className="flex flex-col md:flex-row gap-4 w-full">
                             <div className="flex flex-col w-full ">
                                 <h3>Hvem er jeg i teamet</h3>
                                 <p className="w-5/6">Jeg er den som setter igang ting, lager planer og liker å se resultater. 
@@ -35,25 +35,25 @@ export default function OmMeg(){
 
                         <div className="w-full flex flex-col items-center pt-16 pr-12 gap-12">
                             <h3>Mine styrker</h3>
-                           <div className="w-full flex flex-row justify-evenly items-center">
-                                <div className="flex flex-col items-center gap-3">
+                           <div className="w-full flex flex-col md:flex-row justify-evenly items-center gap-12">
+                                <div className="flex flex-row md:flex-col items-center md:gap-3 gap-7">
                                     <Image src={Briller}/>
-                                    <p className="w-4/6 text-center">Ser hva andre er gode på</p>
+                                    <p className="w-4/6 md:text-center">Ser hva andre er gode på</p>
                                 </div>
 
-                                <div className="flex flex-col items-center gap-3">
+                                <div className="flex flex-row md:flex-col items-center md:gap-3 gap-7">
                                     <Image src={Books}/>
-                                    <p className="w-4/6 text-center">Liker å lære nye ting</p>
+                                    <p className="w-4/6 md:text-center">Liker å lære nye ting</p>
                                 </div>
 
-                                <div className="flex flex-col items-center gap-3">
+                                <div className="flex flex-row md:flex-coll items-center md:gap-3 gap-7">
                                     <Image src={Lys}/>
-                                    <p className="w-4/6 text-center">Kreativ og løsningsorientert</p>
+                                    <p className="w-4/6 md:text-center">Kreativ og løsningsorientert</p>
                                 </div>
 
-                                <div className="flex flex-col items-center gap-3">
+                                <div className="flex flex-row md:flex-col items-center md:gap-3 gap-7">
                                     <Image src={Target}/>
-                                    <p className="w-4/6 text-center">Målrettet, liker å se resultater</p>
+                                    <p className="w-4/6 md:text-center">Målrettet, liker å se resultater</p>
                                 </div>
                            </div>
                         </div>
