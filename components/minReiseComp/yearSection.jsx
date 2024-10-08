7a7bd16562d5044402eb842b6187de1d18562f2c
AndyKodehode <andykodehode@gmail.com>
2024-10-08 17:15:16 +0200

    min reise responsive

--- components/minReiseComp/yearSection.jsx
+++ components/minReiseComp/yearSection.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export function YearCard({bilde, tittel, undertittel, tekst}){
     return(
-        <div className="w-4/6 flex flex-col gap-4">
+        <div className="lg:w-4/6 min-h-full flex flex-col gap-4 p-12 shadow-lg rounded-2xl">
           <h1>{tittel}</h1>
           <h2 className="font-normal">{undertittel}</h2>
           <Image  src={bilde}/>
--- src/app/minReise/page.js
+++ src/app/minReise/page.js
@@ -12,36 +12,35 @@ import { Carousel } from "../../../components/minReiseComp/carousel"
 export default function MinReise(){
     return(
 
-        <div className="p-12 flex flex-col gap-24 items-center ">
-                <div className="flex flex-row w-5/6 justify-center pb-24">
+        <div className="p-12 flex flex-col gap-24 items-center">
+                <div className="flex lg:flex-row flex-col lg:w-5/6 w-full justify-center items-center pb-24 gap-12">
                     <div className=" w-full">
-                        <Image className="w-5/6" src={AnnieSprettReise}/>
+                        <Image className="lg:w-5/6" src={AnnieSprettReise}/>
                     </div>
-                    <div className=" w-3/6 flex flex-col gap-12">
+                    <div className=" w-3/6 flex flex-col items-center gap-12">
                         <h1>Min reise</h1>
-                        <p>
+                        <p className="text-center lg:text-start">
                             Gjennom 4 år har jeg jobbet for å utvikle meg både som designer og utvikler.
                             Jeg har prøvd å lære meg ting både på egenhånd og gjennom kurs.
                         </p>
                     </div>
                 </div>
 
-                <div className="flex flex-row w-full gap-36 p-4 ">
-                    <div className="flex flex-col items-center gap-24 w-full">
+                <div className="flex flex-col w-5/6 lg:gap-40 gap-12 p-4 ">
+                    <div className="flex lg:flex-row lg:items-stretch flex-col lg:justify-center lg:items-start items-center lg:gap-24 gap-12 w-full">
                       <YearCard bilde={Colloseum} tittel="2020" undertittel="XD, Adobe og steaming-prosjekt" tekst="Jeg startet å teste ut webdesign og UX i 2020 ved å lære meg XD, blant annet laget jeg layout for en streaming-service som et eget prosjekt "/>
-                      <YearCard bilde={InnsiktLav} tittel="2022" undertittel="Fontenehuset, app og brukerundersøkelser" tekst="Som et praksisprosjekt i Kodehode begynte jeg på første utgaven for Fontenehus-appen, jeg fikk erfaring med React Native, brukertesting og innsiktsarbeid"/>
-
+                      <YearCard bilde={TipCalculator} tittel="2021" undertittel="Kodehode, Front-end og Figma" tekst="Jeg flyttet tilbake til Bergen og tok etterhvert front-end kurs kalt Kodehode hvor jeg lærte front-end og Figma, før dette hadde jeg også tatt kurs gjennom Interaction Design Foundation"/>
+                     
                     </div >
 
-                    <div className="flex flex-col pt-72 items-center gap-24 w-full">
-                      <YearCard bilde={TipCalculator} tittel="2021" undertittel="Kodehode, Front-end og Figma" tekst="Jeg flyttet tilbake til Bergen og tok etterhvert front-end kurs kalt Kodehode hvor jeg lærte front-end og Figma, før dette hadde jeg også tatt kurs gjennom Interaction Design Foundation"/>
-                      <YearCard bilde={Hawii2} tittel="2023" undertittel="Kodeverket, teamarbeid og reelle prosjekt" tekst="Her fikk jeg erfaring med å jobbe tett sammen med andre, diskutere design, kode og jobbe ut mot reelle kunder"/>
-                        
+                    <div className="flex lg:flex-row lg:items-stretch flex-col lg:justify-center lg:items-start items-center lg:gap-24 gap-12 w-full">
+                      <YearCard bilde={InnsiktLav} tittel="2022" undertittel="Fontenehuset, app og brukerundersøkelser" tekst="Som et praksisprosjekt i Kodehode begynte jeg på første utgaven for Fontenehus-appen, jeg fikk erfaring med React Native, brukertesting og innsiktsarbeid"/>
+                      <YearCard bilde={Hawii2} tittel="2023" undertittel="Kodeverket, teamarbeid og reelle prosjekt" tekst="Her fikk jeg erfaring med å jobbe tett sammen med andre, diskutere design, kode og jobbe ut mot reelle kunder"/>    
                     </div>
 
                 </div>
 
-                <Carousel bilde={Hawii2}/>
+                {/* <Carousel bilde={Hawii2}/> */}
         </div>
     )
 }
