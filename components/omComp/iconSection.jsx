a9e106a42c04f900ede009cf18083a5058c654f5
AndyKodehode <andykodehode@gmail.com>
2025-03-19 11:22:09 +0100

    alignet ikoner på 'om meg'

--- components/omComp/iconSection.jsx
+++ components/omComp/iconSection.jsx
@@ -3,9 +3,13 @@ import Image from "next/image"
 export function AboutIcon({image, text}){
 
     return(
-        <div className="flex flex-row md:flex-col items-center md:gap-3 gap-7 justify-center w-2/6  p-4 rounded-md h-48 w-48 shadow-md">
-            <Image src={image}/>
-            <p className="min-w-24 md:text-center">{text}</p>
+        <div className="flex flex-row md:flex-col items-center md:gap-3 gap-7 justify-center  p-4 rounded-md h-48 w-48 shadow-md">
+            <div className="flex h-24 items-center">
+              <Image src={image}/>
+            </div>
+            <div className="flex items-start h-16">
+              <p className="min-w-24 md:text-center">{text}</p>
+            </div>
       </div>
     )
 }
